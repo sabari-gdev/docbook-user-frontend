01f35250a55cfa69a04ec0d049379abb811cad63
Sabarinath <sabari@Sabarinaths-MacBook-Pro.local>
2024-10-02 19:30:21 +0530

    setup: Initial project setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { MantineProvider } from "@mantine/core";
+
+import "@mantine/core/styles.css";
+
+const App = () => {
+  return (
+    <MantineProvider>
+      <div>App</div>
+    </MantineProvider>
+  );
+};
+
+export default App;
